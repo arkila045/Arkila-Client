e2530f4a503a8d222e13ed0b6679288fe375b348
iamflorenz08 <dejesusflorenz8@gmail.com>
2024-02-06 22:57:42 +0800

    2024.02.06.2257

--- src/app/(main)/guide/page.tsx
+++ src/app/(main)/guide/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react'
 
-export default function page() {
+export default function Page() {
     const [option, setOption] = useState<'renter' | 'owner'>('renter')
     return (
         <section className='py-[120px]'>
